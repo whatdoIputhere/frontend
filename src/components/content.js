3df13ef75506dd6974798aea5367a608ef1b2e7a
whatdoIputhere <pedromgc21@gmail.com>
2024-07-02 10:31:37 +0100

    add dockerfile and deployment yaml

--- src/components/content.js
+++ src/components/content.js
@@ -5,53 +5,61 @@ function Content() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        const url = "http://localhost:3001/notification";
+        const url = "app-service.default.svc.cluster.local:3001/notification";
         axios
             .get(url)
             .then((response) => {
                 setData(response.data);
             })
             .catch((error) => {
+                document.getElementById("err").innerHTML = "Could not fetch data";
                 console.error(error);
             });
     }, []);
 
     const deleteNotification = (email, url) => {
+        document.getElementById("err").innerHTML = "";
         axios
-            .delete("http://localhost:3001/notification/byemailurl", { data: { email, url } })
+            .delete("app-service.default.svc.cluster.local:3001/notification/byemailurl", { data: { email, url } })
             .then(() => {
-            let newData = data.filter((item) => item.email !== email || item.url !== url);
-            setData(newData);
+                let newData = data.filter((item) => item.email !== email || item.url !== url);
+                setData(newData);
             })
             .catch((error) => {
-            console.error(error);
+                document.getElementById("err").innerHTML = "Could not delete notification";
+                console.error(error);
             });
-    }
+    };
 
     const deleteAllNotifications = () => {
+        document.getElementById("err").innerHTML = "";
         axios
-            .delete("http://localhost:3001/notification")
+            .delete("app-service.default.svc.cluster.local:3001/notification")
             .then(() => {
                 setData([]);
             })
             .catch((error) => {
+                document.getElementById("err").innerHTML = "Could not delete notifications";
                 console.error(error);
             });
-    }
+    };
 
     const addNotification = (e) => {
+        document.getElementById("err").innerHTML = "";
         e.preventDefault();
         const url = document.getElementById("url").value;
         const email = document.getElementById("email").value;
         axios
-            .post("http://localhost:3001/notification", { url, email })
+            .post("app-service.default.svc.cluster.local:3001/notification", { url, email })
             .then((response) => {
-                let newData = [...data, {url,email}];
+                let newData = [...data, { url, email }];
                 setData(newData);
             })
             .catch((error) => {
+                document.getElementById("err").innerHTML = "Could not add notification";
                 console.error(error);
             });
+        document.getElementById("url").value = "";
     };
 
     return (
@@ -70,10 +78,16 @@ function Content() {
                     </label>
                 </div>
 
-                <button value="Submit" onClick={addNotification}>Create</button>
-                <button value="clear" onClick={deleteAllNotifications}>Clear</button>
+                <button value="Submit" onClick={addNotification}>
+                    Create
+                </button>
+                <button value="clear" onClick={deleteAllNotifications}>
+                    Clear
+                </button>
             </form>
-
+            <div>
+                <span id="err" className="error"></span>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -82,13 +96,20 @@ function Content() {
                     </tr>
                 </thead>
                 <tbody>
-                    {data && data.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.email}</td>
-                            <td>{item.url}</td>
-                            <td><button onClick={() => deleteNotification(item.email,item.url)}>delete</button></td>
-                        </tr>
-                    ))}
+                    {data &&
+                        data.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.email}</td>
+                                <td>{item.url}</td>
+                                <td>
+                                    <button
+                                        onClick={() => deleteNotification(item.email, item.url)}
+                                    >
+                                        delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                 </tbody>
             </table>
         </div>
